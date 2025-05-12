05d0bcbdb9718fb060e36b20481dd775ee3dc418
Marion_bialgues <marion.bialgues@gmail.com>
2025-05-12 11:54:44 +0200

    soucis espace Competences OK + tableau compétences OK

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
     e.preventDefault();
     alert("Merci pour votre message !");
   });
+
+  function openPDFPopup(pdfUrl) {
+    const popup = document.getElementById('pdfPopup');
+    const frame = document.getElementById('pdfFrame');
+    frame.src = pdfUrl;
+    popup.style.display = 'block';
+  }
+
+  function closePDFPopup() {
+    const popup = document.getElementById('pdfPopup');
+    const frame = document.getElementById('pdfFrame');
+    frame.src = ''; // vide l'iframe
+    popup.style.display = 'none';
+  }
